04625b90c9c4bb02e245ce4f2511aa7b861fdcba
Nightwing-Aslan <aslanarsalan2004@yahoo.com>
2025-06-11 22:44:42 +0100

    Update masterPage.js

--- src/masterPage.js
+++ src/masterPage.js
@@ -2,5 +2,10 @@ import { initDailyReset } from 'backend/public-api';
 
 $w.onReady(() => {
     // Initialize cron job once
+    const styleLink = document.createElement('link');
+    styleLink.rel = 'stylesheet';
+    styleLink.href = '/public/global-styles.css'; // Path to your CSS file
+    document.head.appendChild(styleLink);
+    
     initDailyReset();
 });
