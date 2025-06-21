d05ebbf54a149661482e92c17667bd80a3d5208b
Nightwing-Aslan <aslanarsalan2004@yahoo.com>
2025-06-21 05:16:53 +0100

    Update GameOverLightbox.js

--- src/lightboxes/GameOverLightbox.js
+++ src/lightboxes/GameOverLightbox.js
@@ -1,8 +1,24 @@
 import wixWindow from 'wix-window';
 
 $w.onReady(() => {
-    $w('#tryAgainButton').onClick(() => {
-        wixWindow.closeLightbox();
-        wixLocation.to("/riddles");
-    });
+    const context = wixWindow.lightbox.getContext();
+    const riddles = context.riddles;
+    
+    $w('#repeater').data = riddles;
+    
+    // Show max streak
+    wixData.query("UserStats")
+        .eq("userId", currentUser.id)
+        .find()
+        .then(({ items }) => {
+            if (items.length > 0) {
+                $w('#maxStreak').text = items[0].maxStreak.toString();
+            }
+        });
+});
+
+$w('#repeater').onItemReady(($item, riddle, index) => {
+    $item('#question').text = `Riddle ${index + 1}: ${riddle.question}`;
+    $item('#answer').text = `Answer: ${riddle.answer}`;
+    $item('#explanation').text = `Explanation: ${riddle.explanation}`;
 });
