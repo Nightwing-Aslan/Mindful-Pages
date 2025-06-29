10616dc592766a88f25d6b50dba2f6672b84591a
Ben Gotts <gottsie11@gmail.com>
2025-06-29 15:12:43 +0100

    Fixed UK date again

--- src/public/DateUtils.js
+++ src/public/DateUtils.js
@@ -6,8 +6,11 @@ export function getUKDate() {
 }
 
 export function getUKDateAsString() {
-    const ukNow = new Date(
-        new Date().toLocaleString("en-GB", { timeZone: "Europe/London" })
-    );
-    return ukNow.toISOString().split("T")[0]; // e.g. "2025-06-29"
+    // UK time (UTC+0/UTC+1 for DST)
+    const now = new Date();
+    const isDST = now.getMonth() > 2 && now.getMonth() < 10; // Apr-Oct
+    const ukOffset = isDST ? 60 : 0; // Minutes
+    return new Date(now.getTime() + (ukOffset * 60 * 1000))
+        .toISOString()
+        .split('T')[0];
 }
